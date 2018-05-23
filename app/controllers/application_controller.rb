aa6894ab8ec7a81be615eedefb21f8b42ff7bb70
Chris Stelzer <chrisstelzer@Chriss-iMac.local>
2018-05-23 13:16:22 -0600

    generated new gem into rails app for copyright

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  #Ideal to put items dealing with data here, see includes below
   include DeviseWhitelist
   include SetSource
   include CurrentUserConcern
