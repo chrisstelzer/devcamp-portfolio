552fbf0086cea80cc1c64e3733974336181700af
Chris Stelzer <chrisstelzer@Chriss-iMac.local>
2018-06-08 12:47:14 -0600

    Integrated production calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://chris-stelzer-portfolio.herokuapp.com', 'http://chris-stelzer-portfolio.herokuapp.com/']
+  config.action_cable.url = "wss://chris-stelzer-portfolio.herokuapp.com/cable"
 end
